256d97c17ce33a7cd73bbec719ebddf91fe45dbf
chandanbauri <chandanbauri441@gmail.com>
2021-06-24 05:17:26 +0530

    Splash Screen added

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import AuthNavigator from './authNavigator'
-
+import RNBootSplash from "react-native-bootsplash";
 const APP = () => {
+    useEffect(() => {
+        RNBootSplash.hide({ fade: true });
+    })
     return (
         <NavigationContainer>
             <AuthNavigator />
